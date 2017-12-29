31014bb0e7cc49bdd0e883617361785ba7939c33
jo-va <vandamme_jonathan@hotmail.com>
2017-12-28 23:55:05 -0500

    Added survey form review and submit

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,17 +4,12 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
-
-const FIELDS = [
-	{ label: 'Survey Title', name: 'title' },
-	{ label: 'Subject Line', name: 'subject' },
-	{ label: 'Email Body', name: 'body' },
-	{ label: 'Recipient List', name: 'emails' }
-];
+import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';
 
 class SurveyForm extends Component {
 	renderFields() {
-		return _.map(FIELDS, ({ label, name }) => {
+		return _.map(formFields, ({ label, name }) => {
 			return (
 				<Field
 					type="text"
@@ -29,7 +24,7 @@ class SurveyForm extends Component {
 	render() {
 		return (
 			<div>
-				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+				<form onSubmit={ this.props.handleSubmit(this.props.onSurveySubmit) }>
 					{ this.renderFields() }
 					<Link to="/surveys" className="red btn-flat white-text">
 						Cancel
@@ -47,23 +42,19 @@ class SurveyForm extends Component {
 function validate(values) {
 	const errors = { };
 
-	if (!values.title) {
-		errors.title = 'You must provide a title';
-	}
-	if (!values.subject) {
-		errors.subject = 'You must provide a subject';
-	}
-	if (!values.body) {
-		errors.body = 'You must provide a body';
-	}
-	if (!values.emails) {
-		errors.emails = 'You must provide a list of recipients';
-	}
+	errors.recipients = validateEmails(values.recipients || '');
+
+	_.each(formFields, ({ name, noValueError }) => {
+		if (!values[name]) {
+			errors[name] = noValueError;
+		}
+	});
 
 	return errors;
 }
 
 export default reduxForm({
 	validate,
-	form: 'surveyForm'
+	form: 'surveyForm',
+	destroyOnUnmount:  false
 })(SurveyForm);
